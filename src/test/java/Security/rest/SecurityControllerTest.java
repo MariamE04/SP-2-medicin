865fc240d2003df5fcb13985745fe101d4d9b71e
MariamE04 <elmirmariam16@gmail.com>
2025-10-22 19:56:24 +0200

    alle test er færdig og virker. er i gang med at lave pipeline

---
+++ src/test/java/Security/rest/SecurityControllerTest.java
@@ -0,0 +1,68 @@
+package Security.rest;
+
+import app.config.ApplicationConfig;
+import io.javalin.Javalin;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SecurityControllerTest {
+    private static Javalin app;
+
+    @BeforeAll
+    static void setup() {
+        System.setProperty("test.env", "true");
+        app = ApplicationConfig.getInstance().startServer(7007);
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 7007;
+        RestAssured.basePath = "/api/medicineTracker";
+    }
+
+    @AfterAll
+    static void tearDown() {
+        ApplicationConfig.stopServer(app);
+    }
+
+    @Test
+    void registerUser() {
+        given()
+                .contentType("application/json")
+                .body("{\"username\":\"mariamTest\",\"password\":\"test123\"}")
+                .when()
+                .post("auth/register")
+                .then()
+                .statusCode(201)
+                .body("username", is("mariamTest"))
+                .body("token", notNullValue());
+    }
+
+    @Test
+    void loginUser() {
+        // Registrer først brugeren
+        given()
+                .contentType("application/json")
+                .body("{\"username\":\"mariamTest\",\"password\":\"test123\"}")
+                .when()
+                .post("auth/register")
+                .then()
+                .statusCode(201);
+
+        // Forsøger derefter at logge ind
+        given()
+                .contentType("application/json")
+                .body("{\"username\":\"mariamTest\",\"password\":\"test123\"}")
+                .when()
+                .post("auth/login")
+                .then()
+                .statusCode(200)
+                .body("token", notNullValue())
+                .body("username", is("mariamTest"));
+    }
+
+}
